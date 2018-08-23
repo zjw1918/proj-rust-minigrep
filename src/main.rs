df297fc3d87b1857a40e088596a59dcb7265279f
Zhou <zjw1918@sina.cn>
2018-08-23 14:33:27 +0800

    minigrep done.
    eprintln!("") 不会讲错误信息重定向>到文件

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use minigrep::Config;
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world >>>");
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("parse args error: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for: {}", config.query);
+    println!("In file: {}\n", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("run error: {}", e);
+        process::exit(1);
+    }
 }
